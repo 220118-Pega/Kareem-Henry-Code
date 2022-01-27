66aa9d8d4f318f03d9b4aa562a8de20e3b927d77
Kareem A. Henry <kahenry@niteowler.com>
2022-01-27 15:07:41 -0800

    repo reorganization and Project0 setup

---
+++ Project0/src/com/ersystem/revature/Tickets.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.ersystem.revature;
+
+/**
+ * @author Kniteowler
+ *
+ */
+public class Tickets {
+
+	/**
+	 * 
+	 */
+	public Tickets() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
