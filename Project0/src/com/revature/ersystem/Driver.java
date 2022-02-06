424ecc07d67e00d57ff8ee44e275a8c628a49840
kahenryx <kahenry@whisor.com>
2022-02-05 18:34:06 -0800

    New structure update for maven

---
+++ Project0/src/com/revature/ersystem/Driver.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.revature.ersystem;
+
+public class Driver {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void addRequest() {
+		// TODO Auto-generated method stub
+
+	}
+	public void openRequest() {
+		// TODO Auto-generated method stub
+
+	}
+	public void closeRequest() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project0/src/com/revature/ersystem/obj/Employee.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.revature.ersystem.obj;
+
+/**
+ * @author Kareem A. Henry
+ *
+ */
+public class Employee {
+
+	/**
+	 * 
+	 */
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
